409a2bb4add317cb8e50339d0e87823c56d4fc85
Tokhirjon <tokhirjon007@gmail.com>
2022-02-16 11:21:42 +0500

    project init

---
+++ src/main/java/uz/train/modelappbookingflights/ModelAppBookingFlightsApplication.java
@@ -0,0 +1,13 @@
+package uz.train.modelappbookingflights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelAppBookingFlightsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModelAppBookingFlightsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/train/modelappbookingflights/ModelAppBookingFlightsApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.train.modelappbookingflights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelAppBookingFlightsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
